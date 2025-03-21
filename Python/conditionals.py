e209bdd91e5efee8a0724d6ba45d3f826ed59e03
JWermelin <jakob.wermelin@gmail.com>
2025-03-21 22:46:12 +0100

    tried some conditionals

---
+++ Python/conditionals.py
@@ -0,0 +1,14 @@
+x = 500
+y = 300
+if x == 500 :
+    print("x equals 500")
+    if y > 250 :
+        print("y is bigger")
+print("nested if loop done")
+
+if x < 250 :
+    print("x is smaller than half")
+else :
+    print("x is bigger than half")
+print("two way conditional done")
+
