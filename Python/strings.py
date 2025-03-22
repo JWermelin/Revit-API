b8a81c412b892801fdbf1d3f2422085c19e0e2c4
JWermelin <jakob.wermelin@gmail.com>
2025-03-22 11:20:21 +0100

    Tried out some common String methods.

---
+++ Python/strings.py
@@ -0,0 +1,11 @@
+x = "string"
+x = str.capitalize(x)
+print(x)
+
+x = str.casefold(x)
+print(x)
+
+y = "Hello!Welcome to Tutorialspoint."
+suffix = "oint."
+result=y.endswith(suffix)
+print("The input string ends with the given suffix:", result)
